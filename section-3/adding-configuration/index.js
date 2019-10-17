e45e63cc171e387079fc11d1d9b801e2bea00966
Mark C. Rogers <mrogers@bctconsulting.com>
2019-10-16 21:05:42 -0700

    Parsed paths

--- section-3/adding-configuration/index.js
+++ section-3/adding-configuration/index.js
@@ -1,1 +1,33 @@
-console.log("Hello World");
+/*
+ * Primary file for the API
+ */
+
+ // Dependencies
+
+ var http = require('http');
+ var url = require('url');
+
+ // Configure
+
+var server = http.createServer(function(req, res) {
+
+    // Get the URL and parse it
+    var parsedURL = url.parse(req.url, true);
+
+    // Get the path
+    var path = parsedURL.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Send the response
+    res.end('Hello World\n');
+
+    // Log the request path
+    console.log('Request recieved on path: ' + trimmedPath);
+
+});
+
+ // Start the server
+
+ server.listen(3000, function() {
+     console.log("Server is listening on Port: 3000");
+ });
